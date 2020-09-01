9c3119dd7e70aa1e63d49b45e795bb56bb68c501
kpapakipos <keegan@papakipos.com>
2020-09-01 13:25:25 -0700

    replace starter screen with Router and nav buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import AboutMeScreen from "screens/AboutMeScreen/AboutMeScreen";
+import HomeScreen from "screens/HomeScreen/HomeScreen";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <AboutMeScreen />
+          </Route>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

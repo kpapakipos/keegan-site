9fd1d3cd1ec77315438d9f2207d2d3c8cf58bd13
kpapakipos <keegan@papakipos.com>
2020-09-10 00:34:01 -0700

    start sending frontend index from base Flask route

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return app.send_static_file('index.html')

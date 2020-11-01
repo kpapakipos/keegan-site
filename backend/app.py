2eb93fb08a4dc2e8f9357f47e3ff5e433575aa3c
kpapakipos <keegan@papakipos.com>
2020-10-31 23:15:25 -0700

    get flask backend to serve frontend file correctly

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
     return app.send_static_file('index.html')

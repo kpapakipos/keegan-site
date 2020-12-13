4b192f89a7d3ae8645e99de9a02ec3f3f872a205
Keegan Papakipos <keegan@papakipos.com>
2020-12-13 13:20:46 -0800

    update node backend file a bit. Not quite working

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile('frontend/build/index.html', {root: path.join(__dirname, "../")});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
